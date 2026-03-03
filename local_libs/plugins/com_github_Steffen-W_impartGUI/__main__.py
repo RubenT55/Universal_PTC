218f0825b56da9321fd2b365b220cccbefc7b64c
RubenT55 <196920997+RubenT55@users.noreply.github.com>
2026-03-03 10:44:19 +0100

    Eliminados los plugins del repositorio para ahorrar espacio

--- local_libs/plugins/com_github_Steffen-W_impartGUI/__main__.py
+++
@@ -1,8 +0,0 @@
-from .impart_action import *
-
-if __name__ == "__main__":
-    app = wx.App()
-    frame = wx.Frame(None, title="KiCad Plugin")
-    frontend = ImpartFrontend()
-    frontend.ShowModal()
-    frontend.Destroy()
